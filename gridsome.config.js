da0ae66eb528db7fd5492a7d9bdd1d828e9d0db3
immu0001 <imran.malik01@icloud.com>
2020-09-18 03:49:36 +0530

    Title changed to dn

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'Data Ninjas 🥷',
   siteDescription: "An open-source framework to generate awesome pages",
   plugins: [
     {
